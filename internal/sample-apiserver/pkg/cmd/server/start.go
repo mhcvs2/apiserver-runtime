293415e88f01920c06210cce9b7cd5c12e93211c
mahongchao <mahongchao.chaotian@bytedance.com>
2024-07-05 13:58:32 +0800

    add table name

--- internal/sample-apiserver/pkg/cmd/server/start.go
+++ internal/sample-apiserver/pkg/cmd/server/start.go
@@ -77,6 +77,9 @@ func NewWardleServerOptionsWithPath(out, errOut io.Writer, path string, versions
 		StdOut: out,
 		StdErr: errOut,
 	}
+	if o.RecommendedOptions.Admission != nil {
+		o.RecommendedOptions.Admission.RecommendedPluginOrder = []string{}
+	}
 	o.RecommendedOptions.Etcd.StorageConfig.EncodeVersioner = schema.GroupVersions(versions)
 	o.RecommendedOptions.Etcd.SkipHealthEndpoints = true
 	return o
